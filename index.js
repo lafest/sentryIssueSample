5a69ebdcbe773de7d01931efbcb62e2a9531029b
lafest <dh.wi@ridi.com>
2022-12-19 13:18:16 +0900

    add: init sentry

--- index.js
+++ index.js
@@ -5,5 +5,15 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import * as Sentry from '@sentry/react-native';
+
+if (!__DEV__) {
+  Sentry.init({
+    dsn: 'https://f2942cfb5c3d40fd8bdebfe463315e10@o4504353722925056.ingest.sentry.io/4504353725022208',
+    // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
+    // We recommend adjusting this value in production.
+    tracesSampleRate: 1.0,
+  });
+}
 
 AppRegistry.registerComponent(appName, () => App);
